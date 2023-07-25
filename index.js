64d56886d0a6f5ddb3e42e11a68b7a715732e40f
JeM0z <126226229+JeM0z@users.noreply.github.com>
2023-07-25 00:45:16 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var nombre= "Jean";
+var edad = 29;
+var EresDesarrollador = true;
+const fechaNacimiento = new Date("December 20 1993");
+var libroFavorito = {
+        titulo:"La Naúsea",
+        autor:"Sartre",
+        fechaDePublicación: 1938,
+        url:"https://www.amazon.com/-/es/Jean-Paul-Sartre/dp/0811220303/ref=sr_1_1?crid=2JMIZ45XZYBO0&keywords=la+nausea+jean+paul+sartre&qid=1690262035&sprefix=la+nausea+%2Caps%2C207&sr=8-1",
+    }
+
+
+console.log(libroFavorito);
+
